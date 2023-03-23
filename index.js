ab40f1b41fc9ea149dcc6f4cb741b820d96aa05c
David Rashid <davidmra@umich.edu>
2023-03-23 13:58:37 -0400

    Finished G

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let num1 = 31, num2 = 2;
+let multiply = num1 * num2;
+
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+console.log(random);
+
+let num3 = 104, num4 = 25;
+let mod = num3 % num4;
+
+console.log(mod);
+
+let max = Math.max(2, 3, 5, 20);
+console.log(max);
+
+
+
 
 // Write your code here
